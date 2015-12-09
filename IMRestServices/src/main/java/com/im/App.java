5fe505a37ca0d305b719ec53f5a64abf98db4f38
mendhe_c <mendhe_c@Chetan-M.India.XoriantCorp.com>
2015-12-09 20:41:51 +0530

    Added annotations to App.java

--- IMRestServices/src/main/java/com/im/App.java
+++ IMRestServices/src/main/java/com/im/App.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableAspectJAutoProxy
+@ComponentScan
 public class App 
 {
     public static void main( String[] args )
