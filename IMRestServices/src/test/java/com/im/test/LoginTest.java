9702ff1695d5ec5647d5c838aa4c24329b102583
gurav_s <gurav_s@Srushti-G.India.XoriantCorp.com>
2015-12-09 12:16:01 +0530

    LoginTest case done

--- IMRestServices/src/test/java/com/im/test/LoginTest.java
+++ IMRestServices/src/test/java/com/im/test/LoginTest.java
@@ -6,25 +6,47 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class LoginTest extends TestCase {
-	
+
 	@Before
-	void setup(){	
+	void setup() {
 	}
-	
-	
-@Test
+
+	@Test
 	public void testLogin() {
-		
-		Client client=Client.create();
-		WebResource webResource=client.resource("http://localhost:8080/login?username=Srushtig&password=srushti");
-		
-		ClientResponse response=webResource.type("application/json").post(ClientResponse.class);
-		
-				String output = response.getEntity(String.class);
-	assertEquals("{\"status\": \"ok\"}",output);
+
+		Client client = Client.create();
+		WebResource webResource = client
+				.resource("http://localhost:8080/login?username=srushti&password=srushti123");
+		String request = webResource.get(String.class);
+		String output = request.toString();
+
+		assertEquals("{\"status\": \"ok\"}", output);
 	}
+	
+	@Test
+	public void testNoSuchUsername() {
+
+		Client client = Client.create();
+		WebResource webResource = client
+				.resource("http://localhost:8080/login?username=srushsdsti&password=srushti123");
+		String request = webResource.get(String.class);
+		String output = request.toString();
+
+		assertEquals("{\"status\":\"-1\"}", output);
+	}
+	@Test
+	public void testWrongCredentials() {
+
+		Client client = Client.create();
+		WebResource webResource = client
+				.resource("http://localhost:8080/login?username=srushti&password=sadasd");
+		String request = webResource.get(String.class);
+		String output = request.toString();
+
+		assertEquals("{\"status\": \"fail\"}", output);
+	}
+
 }
--- IMRestServices/src/test/java/com/im/test/RegistrationTest.java
+++ IMRestServices/src/test/java/com/im/test/RegistrationTest.java
@@ -5,28 +5,31 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class RegistrationTest extends TestCase {
-	
+
 	@Before
-	void setup(){	
+	void setup() {
 	}
+
 	@Test
 	public void testRegister() {
-		
-		Client client=Client.create();
-		WebResource webResource=client.resource("http://localhost:8080/imregister");
-		
-String registerData ="{\"firstName\":\"Srushti\",\"lastName\":\"G\",\"address\":\"something\",\"state\":\"MH\",\"city\":\"something\",\"contact_no\":\"something\",\"email\":\"srushti@gmail.com\",\"username\":\"srushti\",\"password\":\"srushti123\"}";
 
-		ClientResponse response = webResource.type("application/json")
-		   .post(ClientResponse.class,registerData);
+		Client client = Client.create();
+		WebResource webResource = client
+				.resource("http://localhost:8080/imregister");
+		String registerData = "{\"firstName\":\"Srushti\",\"lastName\":\"G\",\"address\":\"something\",\"state\":\"MH\",\"city\":\"something\",\"contact_no\":\"something\",\"email\":\"srushti@gmail.com\",\"username\":\"srushti\",\"password\":\"srushti123\"}";
+
+		ClientResponse response = webResource.type("application/json").post(
+				ClientResponse.class, registerData);
 		String output = response.getEntity(String.class);
-assertEquals("{\"firstName\":\"Srushti\",\"lastName\":\"G\",\"address\":\"something\",\"state\":\"MH\",\"city\":\"something\",\"contact_no\":\"something\",\"email\":\"srushti@gmail.com\",\"username\":\"srushti\",\"password\":\"srushti123\"}",output);
+		assertEquals(
+				"{\"firstName\":\"Srushti\",\"lastName\":\"G\",\"address\":\"something\",\"state\":\"MH\",\"city\":\"something\",\"contact_no\":\"something\",\"email\":\"srushti@gmail.com\",\"username\":\"srushti\",\"password\":\"srushti123\"}",
+				output);
+		
 	}
 
 }
