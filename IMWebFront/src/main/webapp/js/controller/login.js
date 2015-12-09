36630065ad8a6c0a194fbe54200c834ebfe129c1
soni_k <soni_k@KANAK-S.India.XoriantCorp.com>
2015-12-09 14:53:42 +0530

    updated login.js

--- IMWebFront/src/main/webapp/js/controller/login.js
+++ IMWebFront/src/main/webapp/js/controller/login.js
@@ -12,19 +12,20 @@ register.controller("LoginCtrl",function($scope,$http,$resource,$location){
 	$scope.loginerrorshow=false;
 
 	var result=$http.get(baseUrl+"/login?userName=" + $scope.userName + "&password=" + $scope.password).success(function(response)
-	{
-		if(response.status=="ok")
 			{
-			$location.path("dashboard");
-			}
-		else
-			{
-			$scope.loginerrorshow=true;
-			}
-	}		
-	).error(function(response){$scope.loginerrorshow=true;});
+				var userDetails = response.userDetails;
+				if(userDetails!=null)
+					{
+					$location.path("dashboard");
+					}
+				else
+					{
+					$scope.loginerrorshow=true;
+					}
+			}		
+			).error(function(response){$scope.loginerrorshow=true;});
 
-	}
+			}
 });
 
 
