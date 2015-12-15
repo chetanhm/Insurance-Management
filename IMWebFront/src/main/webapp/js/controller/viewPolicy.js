fdd573910d58f678a466406b434cdfd49b19bcbf
soni_k <soni_k@KANAK-S.India.XoriantCorp.com>
2015-12-15 13:18:40 +0530

    changes in admin view all policies

--- IMWebFront/src/main/webapp/js/controller/viewPolicy.js
+++ IMWebFront/src/main/webapp/js/controller/viewPolicy.js
@@ -5,16 +5,14 @@ var allPolicy = angular.module("allPolicies", []);
 
 allPolicy.controller("allPolicyCtrl",function($scope,$http)
 {	
+	
 	var result=$http.get(baseUrl+"/policy").success(function(response)
-	{
+	{		
 		$scope.policyList=response;
 	});
 	$scope.setStatus= function(status,policyNumber)
 	{
-		console.log("status"+status);
-		console.log("policyNumber"+policyNumber);
-		$http.put(baseUrl+"/policy?policyNumber="+policyNumber+"&status="+status);
-		
+			$http.put(baseUrl+"/policy?policyNumber="+policyNumber+"&status="+status);		
 	};
 });
 })();
