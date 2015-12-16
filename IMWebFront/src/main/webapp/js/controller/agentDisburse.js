71d9a31a0d8631961a87f32599cb09ca7c01c80d
gurav_s <gurav_s@Srushti-G.India.XoriantCorp.com>
2015-12-16 21:58:58 +0530

    Changes in disbursement by agent

--- IMWebFront/src/main/webapp/js/controller/agentDisburse.js
+++ IMWebFront/src/main/webapp/js/controller/agentDisburse.js
@@ -4,18 +4,19 @@
 var agent = angular.module("agentDisburse", []);
 
 agent.controller("AgentDisburseCtrl",function($scope,$stateParams,$http){	
+
 	var result=$http.get(baseUrl+"/claim?userName="+$stateParams.userName).success(function(response){
 	
 		var loadUser=JSON.parse(localStorage.getItem("user"));
 		$scope.customerName=$stateParams.userName;
 		$scope.claimDetailsList=response;
-		if(response.status=='Ok')
-		{
-		$scope.hideTable=false;
-		}
+		if($scope.claimDetailsList=='')
+			{
+			$scope.showSuccess=false;
+			$scope.hideTable=true;
+			}
 		else
 			{
-			$scope.hideTable=true;
 			$scope.showSuccess=true;
 			}
 		
