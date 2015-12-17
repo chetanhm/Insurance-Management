1803ba74f0a8a6780a52028ba25b5d0eee6e7d70
gurav_s <gurav_s@Srushti-G.India.XoriantCorp.com>
2015-12-17 15:02:14 +0530

    Changes in all policies and assess disbursement by direct customer

--- IMWebFront/src/main/webapp/js/controller/disburse.js
+++ IMWebFront/src/main/webapp/js/controller/disburse.js
@@ -9,6 +9,16 @@ disburse.controller("DisburseCtrl",function($scope,$http){
 	var result=$http.get(baseUrl+"/claim?userName="+loadUser.userName).success(function(response){
 		$scope.userName=loadUser.userName;
 		$scope.claimDetailsList=response;
+		if($scope.claimDetailsList=='')
+		{
+		$scope.hideTable=true;
+		$scope.showSuccess=false;
+		}
+	else
+		{
+		$scope.hideTable=false;
+		$scope.showSuccess=true;
+		}
 	});
 	
 });
--- IMWebFront/src/main/webapp/js/controller/policy.js
+++ IMWebFront/src/main/webapp/js/controller/policy.js
@@ -8,16 +8,29 @@
 	
 		var result=$http.get(baseUrl+"/policy/"+loadUser.userName).success(function(response)
 		{
+			$scope.allPolicies=response;
 			if(response==[])
 				{
 					$scope.user="No policies";
 					$scope.status="Fail";
+					
 				}
 			else
 				{
 					$scope.user=loadUser.userName;
 					$scope.listPolicies=response;
 					$scope.status="Pass";
+				
+				}
+			if($scope.allPolicies=='')
+				{
+				$scope.hideTable=true;
+				$scope.showSuccess=false;
+				}
+			else
+				{
+				$scope.hideTable=false;
+				$scope.showSuccess=true;
 				}
 		});				
 		
