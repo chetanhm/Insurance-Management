e8ae83ad5235eee12ec0ea588184d8e3e0bb65c6
mendhe_c <mendhe_c@Chetan-M.India.XoriantCorp.com>
2015-12-17 19:49:05 +0530

    Change of REST call /claim/type/total

--- IMWebFront/src/main/webapp/js/controller/imcharts.js
+++ IMWebFront/src/main/webapp/js/controller/imcharts.js
@@ -5,21 +5,20 @@ var imcharts = angular.module("imcharts", []);
 
 imcharts.controller("ChartCtrl",function($scope,$http,$state){	
 	
-	$http.get(baseUrl+"/policy/status/all").success(function(response){
+	$http.get(baseUrl+"/policy/status/total").success(function(response){
 		$scope.policyLabels=response.labels;
 		$scope.policyData=response.data;
 		
 		
 	});
 	
-	$http.get(baseUrl+"/claim/type/all").success(function(response){
+	$http.get(baseUrl+"/claim/type/total").success(function(response){
 		$scope.claimLabels=response.labels;
 		$scope.claimData=response.data;
 		
 		
 	});
-//	  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-//	  $scope.data = [300, 500, 100];
+
 	  
 	  
 });
